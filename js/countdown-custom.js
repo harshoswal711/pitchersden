2f35bd4fa8d9f0681b7a7484a6bf8453fde5e31d
darshantholiya <darshantholiya036@gmail.com>
2022-03-19 10:36:50 +0530

    organisers , schedule , favicon details updated

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 02, 20, 12)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022, 03, 20, 12)}); // year, month, date, hour
         });
 });		
 
