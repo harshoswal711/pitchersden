0860fedba97688efe0631d44b8c46830b8d1dfad
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-03-21 14:46:02 +0530

    Update countdown-custom.js

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 03, 20, 12)}); // year, month, date, hour
-        });
-});		
+jQuery(document).ready(function () {
+    $(function () {
+        $('#defaultCountdown').countdown({ until: new Date(2022, 03, 21, 12) }); // year, month, date, hour
+    });
+});
 
